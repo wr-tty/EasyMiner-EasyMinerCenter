3ffa9a266709a78cfa5e76c220b4fe799ca6f875
R W <r@w.cz>
2019-01-03 13:42:55 +0100

    EasyMinerCenter-Docker: start.sh react on db.php return code

--- docker/db.php
+++ docker/db.php
@@ -1,19 +1,31 @@
 <?php
 
+function exception_error_handler($severity, $message, $file, $line) {
+    throw new ErrorException($message, 1, $severity, $file, $line);
+}
+set_error_handler("exception_error_handler");
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 $databasePrefix = $argv[1];
 $database = $databasePrefix . '000';
 
-$mysqli = new mysqli("easyminer-mysql", "root", "root");
-$mysqli->query('DROP DATABASE IF EXISTS '.$database);
-$mysqli->query('GRANT ALL PRIVILEGES ON *.* TO "'.$database.'"@"%" IDENTIFIED BY "'.$database.'" WITH GRANT OPTION');
-$mysqli->query('CREATE DATABASE '.$database);
-$mysqli->query("GRANT ALL PRIVILEGES ON ".$database.".* TO '".$database."'@'%' IDENTIFIED BY '".$database."' WITH GRANT OPTION");
-$mysqli->select_db($database);
+try {
+    $mysqli = new mysqli("easyminer-mysql", "root", "root");
+    $mysqli->query('DROP DATABASE IF EXISTS '.$database);
+    $mysqli->query('GRANT ALL PRIVILEGES ON *.* TO "'.$database.'"@"%" IDENTIFIED BY "'.$database.'" WITH GRANT OPTION');
+    $mysqli->query('CREATE DATABASE '.$database);
+    $mysqli->query("GRANT ALL PRIVILEGES ON ".$database.".* TO '".$database."'@'%' IDENTIFIED BY '".$database."' WITH GRANT OPTION");
+    $mysqli->select_db($database);
 
-$sql = file_get_contents('/var/www/html/easyminercenter/app/InstallModule/data/mysql.sql');
-$mysqli->multi_query($sql);
+    $sql = file_get_contents('/var/www/html/easyminercenter/app/InstallModule/data/mysql.sql');
+    $mysqli->multi_query($sql);
 
-$mysqli->close();
+    $mysqli->close();
+} catch(Exception $e) {
+    echo 'Message: ' . $e->getMessage() . "\n";
+    echo 'File: ' . $e->getFile() . "\n";
+    echo 'Line: ' . $e->getLine() . "\n";
+    exit($e->getCode());
+}
--- docker/start.sh
+++ docker/start.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 export DBPREFIX="$(pwgen 6 1)_"
+ISDBREADY=1
 
 sed -i -- "s/emc_/$DBPREFIX/g" /var/www/html/easyminercenter/app/config/config.local.neon
 
-php /root/db.php $DBPREFIX
+while [ $ISDBREADY -ne 0 ]; do
+    php /root/db.php $DBPREFIX
+    let ISDBREADY=$?
+    if [ $ISDBREADY -ne 0 ]
+    then
+        echo "Unsuccessful database creation. Waiting 2 seconds for next attempt..."
+        sleep 2s
+    fi
+done
 
 apache2-foreground
